5f01107aea25eea7a7a8cf9eedbcb6a1c07b77d9
Dima <d9gm87@gmail.com>
2023-09-13 23:06:24 -0700

    init

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,4 +3,4 @@ git add .
 git commit -m init
 git push origin master
 git push azure master
-open https://dev.azure.com/NiceCompany/Project/_build?definitionId=20&_a=summary
+open https://dev.azure.com/Company/Project/_build?definitionId=20&_a=summary
